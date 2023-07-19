5663628e89e5fbf0f2142454c0236f341cbe0402
Rinto <satorugojo4200@gmail.com>
2023-07-19 03:07:32 +0000

    RMX2020: target_excludes_audiofx true
    Because We RealmeDirac. We don't need extra Sound Modifications app

--- device.mk
+++ device.mk
@@ -27,6 +27,9 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 # Dynamic Partition
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+#AudioFX
+TARGET_EXCLUDES_AUDIOFX := true
+
 # APN
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
